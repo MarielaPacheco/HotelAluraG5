0bf05c6f864abc4e59d222394befa5fbcdb959e9
MarielaPacheco <130937474+MarielaPacheco@users.noreply.github.com>
2023-09-23 15:06:53 -0300

    Add files via upload

---
+++ basedatoshotel.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE hotel_alura;
+ use hotel_alura;
+ 
+ create table reservas(
+ id int not null auto_increment,
+ fecha_entrada date not null,
+ fecha_salida date not null,
+ valor varchar (50),
+ forma_de_pago varchar (50) not null,
+ PRIMARY KEY (id)
+);
+
+create table huespedes(
+ id int not null auto_increment,
+ nombre varchar (50) not null,
+ apellido varchar (50) not null,
+ fecha_de_nacimiento date not null,
+ nacionalidad varchar (50) not null,
+ telefono varchar (50) not null,
+ id_reserva int not null,
+ primary key (id),
+ foreign key (id_reserva) references reservas(id)
+);
+
+create table usuarios(
+ nombre varchar (50),
+ contraseña varchar (50)
+);
+
+insert into usuarios (nombre, contraseña) values ('Mariela','123456');
+insert into usuarios (nombre, contraseña) values ('admin','admin');
+
+
+ 
---
+++ src/factory/conexionBase.java
@@ -0,0 +1,30 @@
+package factory;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class conexionBase {
+public DataSource datasource;
+
+
+public conexionBase() {
+	ComboPooledDataSource comboPool = new ComboPooledDataSource();
+	comboPool.setJdbcUrl("jdbc:mysql://localhost/3306");
+	comboPool.setUser("root");
+	comboPool.setPassword("123456");
+	
+	this.datasource = comboPool;
+}
+public Connection recuperarConexion() {
+	try {
+		return this.datasource.getConnection();
+	} catch (SQLException e) {
+		throw new RuntimeException(e);
+	}
+}
+}
